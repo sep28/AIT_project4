63f16ca309503ce47db8f87eae4b68ff8e850a04
Sep <sepmacbook@10-16-140-89.dynapool.wireless.nyu.edu>
2019-10-09 17:29:16 -0400

    finished part2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const handlebars = require('handlebars');
+
+app.set('view engine', 'hbs');
+
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.use()
+app.get("/", (req, res) => {
+	res.render("home")});
+
+app.listen(3000);
+console.log("Server started; type CTRL+C to shut down\n");
