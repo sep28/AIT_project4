ad8f165cafe3b609cfdfa620ca4effa0b9915981
Sep <sepmacbook@10-16-140-89.dynapool.wireless.nyu.edu>
2019-10-09 17:44:51 -0400

    finished part3

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const handlebars = require('handlebars');
 app.set('view engine', 'hbs');
 
 const publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
 
-app.use()
+app.use(express.urlencoded());
+app.use(function(req, res, next) {
+	console.log("Method: " + req.method + "\n");
+	console.log("Path: " + req.path + "\n");
+	console.log("Query: " + req.query + "\n");
+	next();
+});
+app.use(express.static(publicPath));
 app.get("/", (req, res) => {
 	res.render("home")});
 
